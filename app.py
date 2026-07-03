a7d3bc79e1ed1ecd0ce3f9a1a6aba852a0b35662
T9113 <tayyabmasood911@gmail.com>
2026-07-03 15:32:44 +0500

    update V2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello():
     <html>
         <head><title>AWS Serverless CI/CD Complete</title></head>
         <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 100px;">
-            <h1 style="color: #2E86C1;">🚀 CodePipeline Deployment Successful!</h1>
+            <h1 style="color: #2E86C1;">🚀 CodePipeline Deployment Successful! V2</h1>
             <p style="font-size: 18px;">Your Python application is live on AWS EC2 without CodeDeploy Agent dependency chains.</p>
         </body>
     </html>
